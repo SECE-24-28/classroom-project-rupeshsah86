62cfbfa6bea0b28e4673ada72bf2e4f0b0f6d082
rupeshsah86 <rupeshkumarsah.2024cse@sece.ac.in>
2025-12-02 11:26:57 +0530

    Adding first files

---
+++ src/Hello-world/hello-world-component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HelloWorldStyle } from "./hello-world-style";
+const HelloWorldComponent = () => {
+  return (
+    <HelloWorldStyle>
+      <h1 className="element">Sri eshwar</h1>
+    </HelloWorldStyle>
+  );
+};
+export default HelloWorldComponent;
