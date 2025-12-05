9887ea1ef63b4249e056d1594cbd18f112ec9df1
rupeshsah86 <rupeshkumarsah.2024cse@sece.ac.in>
2025-12-05 12:58:39 +0530

    Adding day 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,13 @@
-// // import { useState } from 'react'
-// // import reactLogo from './assets/react.svg'
-// // import viteLogo from '/vite.svg'
-// // import './App.css'
-// // import HelloWorldComponent from "./Hello-World-Component";
-
-
-// // function App() {
-// //   const [count, setCount] = useState(0)
-
-// //   return (
-// //     <>
-// //       <div>
-// //         <a href="https://vite.dev" target="_blank">
-// //           <img src={viteLogo} className="logo" alt="Vite logo" />
-// //         </a>
-// //         <a href="https://react.dev" target="_blank">
-// //           <img src={reactLogo} className="logo react" alt="React logo" />
-// //         </a>
-// //       </div>
-
-// //       <h1>Hello Rupesh! React is Running 🚀</h1>
-
-// //       {/* 👉 Use your new component */}
-// //       <Hello />
-
-// //       <div className="card">
-// //         <button onClick={() => setCount((count) => count + 1)}>
-// //           Clicked {count} times
-// //         </button>
-// //         <p>
-// //           Edit <code>src/App.jsx</code> and save to test HMR
-// //         </p>
-// //       </div>
-
-// //       <p className="read-the-docs">
-// //         Click on the Vite and React logos to learn more
-// //       </p>
-// //     </>
-// //   )
-// // }
-
-// // export default App
-// import './App.css';
-// import HelloWorldComponent from './Hello-world/hello-world-component';
-// import ProfessionalForm from './Hello-world/ProfessionalForm';
-
-// function App() {
-//   return (
-//     <div className="app-container">
-//       <header className="app-header">
-//         <HelloWorldComponent name="My React App" title="WelCome" />
-//       </header>
-//       <main className="app-main">
-//         <ProfessionalForm 
-//           title="Professional Registration Form"
-//           onSubmit={(data) => console.log('Form Data:', data)}
-//         />
-//       </main>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import "./App.css";
 import FlexBoxComponent from "./flex-box/flex-box-component";
-import HelloWorldComponent from "./Hello-world/hello-world-component";
+import FlexWrapComponent from "./flex-wrap/flex-wrap-component";
+import HelloWorldComponent from "./hello-world/hello-world-component";
 import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
-import SymbolOfExcellenceComponent from "./symbol-of-excellence/symbol-of-excellence-component";
+import OurPromisesComponent from "./our-promises/our-promises";
+import PositionExampleComponent from "./position-example/position-example-component";
+import PostionComponent from "./positioning/position-component";
+import SymbolOfExcellenceComponent from "./symbol-of-excellence/symbol-of-execllence-component";
+import TrustedbyComponent from "./trusted-by/trusted-by-component";
 
 function App() {
   return (
@@ -76,8 +15,12 @@ function App() {
       {/* <HelloWorldComponent /> */}
       {/* <FlexBoxComponent /> */}
       {/* <LeadersTomorrowComponent /> */}
-      <SymbolOfExcellenceComponent />
-
+      {/* <SymbolOfExcellenceComponent /> */}
+      {/* <PostionComponent /> */}
+      {/* <TrustedbyComponent /> */}
+      {/* <FlexWrapComponent /> */}
+      {/* <OurPromisesComponent /> */}
+      <PositionExampleComponent />
     </div>
   );
 }
