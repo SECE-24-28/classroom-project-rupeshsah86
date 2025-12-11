896783f84cc742e2027e683e3ceef20f0ca33d5b
rupeshsah86 <rupeshkumarsah.2024cse@sece.ac.in>
2025-12-11 12:46:55 +0530

    Adding post operation with working backend

--- src/signup-form-api/signup-form-component.jsx
+++ src/signup-form-api/signup-form-component.jsx
@@ -1,39 +1,68 @@
 import React from "react";
 import axios from "axios";
+
 const SignupFormComponent = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
-    // console.log("=>(The value:)>=", e.target.email.value);
-    // console.log("The first name:", e.target.firstname.value);
     try {
-      console.log("CHECK ENTRY 2381");
+      console.log("Submitting...");
 
       const pushDetails = await axios.post(
         "http://localhost:21000/api/v1/User/userSignup",
         {
           firstName: e.target.firstname.value,
+          lastName: e.target.lastname.value,
           email: e.target.email.value,
+          mobileNumber: e.target.mobile.value,
+          password: e.target.password.value,
         }
       );
-      if (pushDetails.status === 200) {
-        console.log("The process is successful");
+
+      if (pushDetails.data.success === true) {
+        alert("User registered successfully!");
       } else {
-        console.log("Error occured");
+        alert("Something went wrong!");
       }
     } catch (e) {
-      console.log("Error:", e);
+      alert("Backend not running or request failed.");
+      console.log("Error:", e.message);
     }
   };
+
   return (
-    <div>
+    <div style={{ padding: "30px" }}>
+      <h2>Signup Form</h2>
+
       <form onSubmit={onSubmit}>
+        <label htmlFor="firstname">First Name</label>
+        <input type="text" id="firstname" required />
+
+        <br /><br />
+
+        <label htmlFor="lastname">Last Name</label>
+        <input type="text" id="lastname" />
+
+        <br /><br />
+
         <label htmlFor="email">Email</label>
-        <input type="text" id="email" />
-        <label htmlFor="firstname">First name</label>
-        <input type="text" id="firstname" />
+        <input type="email" id="email" required />
+
+        <br /><br />
+
+        <label htmlFor="mobile">Mobile Number</label>
+        <input type="text" id="mobile" required />
+
+        <br /><br />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" required />
+
+        <br /><br />
+
         <button type="submit">Submit</button>
       </form>
     </div>
   );
 };
+
 export default SignupFormComponent;
